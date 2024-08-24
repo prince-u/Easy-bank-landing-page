09fc4ef89ccd9c8afd0b67d72069b502d1423a1f
Ugo <ugochukwuobianuonye@gmail.com>
2024-08-24 14:17:31 +0100

    Adding intersection observer and now I'm doneeeeeeee!!!!

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//This is for handling the dropdown menu
+
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const dropDownMenu = document.querySelector(".nav-links");
 const fourPhonesImg = document.querySelector(".four-phones>img");
@@ -21,4 +23,28 @@ darkOverlay.addEventListener("click", () => {
   darkOverlay.classList.remove("active");
   html.classList.remove("active");
   hamburgerIcon.innerHTML = `<img src="images/icon-hamburger.svg" alt="">`;
+});
+
+//This is for the intersection observer
+
+const main2 = document.querySelector(".container2>main");
+const main3 = document.querySelector(".container3>main");
+const benefitsImage = document.querySelectorAll(".container3 .benefits-image");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: "0.5" }
+);
+
+observer.observe(main2);
+observer.observe(main3);
+benefitsImage.forEach((el) => {
+  observer.observe(el);
 });
